0f2e7e7ca772859232137635b9d3676a73f7cc44
brinks92 <Cheeto1918>
2019-03-20 23:44:48 -0700

    Should be working

--- app/src/main/java/edu/cascadia/mobile/apps/movies/EditMovie.java
+++ app/src/main/java/edu/cascadia/mobile/apps/movies/EditMovie.java
@@ -1,5 +1,6 @@
 package edu.cascadia.mobile.apps.movies;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,26 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import edu.cascadia.mobile.apps.movies.database.movieDatabase;;
 import edu.cascadia.mobile.apps.movies.model.MovieEntity;
 
 public class EditMovie extends AppCompatActivity {
     private movieDatabase mDatabase;
+    public static String MOVIE_ID_KEY = "id";
 
+    private MovieEntity Movie;
+    private int id;
+    private int List;
+    private int run_time;
+    private int secondList;
+
+    private EditText eTitle = findViewById(R.id.edit_title);
+    private EditText eDirector = findViewById(R.id.edit_director);
+    private EditText eYear = findViewById(R.id.edit_year);
+    private EditText eRuntime = findViewById(R.id.edit_runtime);
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +38,23 @@ public class EditMovie extends AppCompatActivity {
 
         mDatabase = movieDatabase.getInstance(this);
 
+        List = mDatabase.movieDao().getCount();
+
+
+
+        Movie = mDatabase.movieDao().getMovie(getIntent().getIntExtra(MOVIE_ID_KEY,0));
+
+
+
+        if (Movie != null) {
+            eDirector.setText(Movie.getDirector());
+            eTitle.setText(Movie.getTitle());
+            id = Movie.getId();
+            eRuntime.setText(Integer.toString(Movie.getRunTime()));
+            eYear.setText(Movie.getYear());
+        }
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,23 +63,33 @@ public class EditMovie extends AppCompatActivity {
                 int movieId = 0;
 
                 //Add Movie
-                EditText eTitle = findViewById(R.id.edit_title);
-                EditText eDirector = findViewById(R.id.edit_director);
-                EditText eYear = findViewById(R.id.edit_year);
-                EditText eRuntime = findViewById(R.id.edit_runtime);
-                int runtime = Integer.parseInt(eRuntime.getText().toString());
+
+                run_time = Integer.parseInt(eRuntime.getText().toString());
 
                 MovieEntity mMovie = new MovieEntity(
                         movieId,
                         eTitle.getText().toString(),
                         eDirector.getText().toString(),
                         eYear.getText().toString(),
-                        runtime
+                        run_time
                         );
 
                 mDatabase.movieDao().addOrUpdate(mMovie);
 
+                secondList = mDatabase.movieDao().getCount();
 
+
+
+                if (List == secondList) {
+
+                    Toast.makeText(EditMovie.this, "Movie Updated", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    Toast.makeText(EditMovie.this, "New Movie Added", Toast.LENGTH_SHORT).show();
+                }
+
+                finish();
                 Snackbar.make(view, "Movie Added to Database", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
--- app/src/main/java/edu/cascadia/mobile/apps/movies/ui/MoviesAdapter.java
+++ app/src/main/java/edu/cascadia/mobile/apps/movies/ui/MoviesAdapter.java
@@ -1,69 +1,163 @@
 package edu.cascadia.mobile.apps.movies.ui;
 
+
+
 import android.content.Context;
+
 import android.content.Intent;
+
+import android.graphics.Movie;
+
 import android.support.annotation.NonNull;
+
 import android.support.design.widget.FloatingActionButton;
+
 import android.support.v7.widget.RecyclerView;
+
 import android.view.LayoutInflater;
+
 import android.view.View;
+
 import android.view.ViewGroup;
+
 import android.widget.TextView;
+
+
 
 import java.util.List;
 
+
+
+import edu.cascadia.mobile.apps.movies.EditMovie;
+
 import edu.cascadia.mobile.apps.movies.R;
+
 import edu.cascadia.mobile.apps.movies.model.MovieEntity;
 
+
+
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
-    private final List<MovieEntity> mMovies;
+
+    private List<MovieEntity> mMovies;
+
     private final Context mContext;
 
+
+
     public MoviesAdapter(List<MovieEntity> mMovies, Context mContext) {
+
         this.mMovies = mMovies;
+
         this.mContext = mContext;
+
     }
+
+
 
     @NonNull
+
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, int viewType) {
+
+    public ViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
+
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
         View view = inflater.inflate(R.layout.movie_list_item, parent, false);
 
-        FloatingActionButton fab = view.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO: pass movie data to the intent
 
-                Intent editMovie = new Intent("edu.cascadia.mobile.apps.movies.EditMovie");
-                parent.getContext().startActivity(editMovie);
-            }
-        });
 
         return new ViewHolder(view);
+
     }
 
+
+
     @Override
+
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        final MovieEntity movie = mMovies.get(position);
 
-        holder.mTextView.setText(movie.getTitle() + ": " + movie.getDirector());
+        holder.bindData(position);
 
     }
+
+
 
     @Override
+
     public int getItemCount() {
+
         return mMovies.size();
+
     }
+
+
+
+    public void updateData(List<MovieEntity> movies) {
+
+        this.mMovies = movies;
+
+        notifyDataSetChanged();
+
+    }
+
+
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView mTextView;
+
+
+        private TextView mTextView;
+
+        private FloatingActionButton Fab;
+
+
 
         public ViewHolder(View itemView){
+
             super(itemView);
+
+
+
             mTextView = itemView.findViewById(R.id.movie_text);
+
+            Fab = itemView.findViewById(R.id.fab);
+
+
+
+            Fab.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+
+                public void onClick(View view) {
+
+                    MovieEntity movie = mMovies.get(getAdapterPosition());
+
+                    Intent editMovie = new Intent(mContext, EditMovie.class);
+
+                    editMovie.putExtra(EditMovie.MOVIE_ID_KEY, movie.getId());
+
+                    mContext.startActivity(editMovie);
+
+
+
+                }
+
+            });
+
         }
+
+
+
+        void bindData(int position) {
+
+            MovieEntity movie = mMovies.get(position);
+
+            mTextView.setText(movie.getTitle() + ": " + movie.getDirector());
+
+        }
+
+
+
     }
+
 }
